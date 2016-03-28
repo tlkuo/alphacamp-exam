b42fc9310ce91f20b36f34ae17356f0f2a698bfc
martin <tlkuo.frb@gmail.com>
2016-03-28 13:25:47 +0800

    Initail xcode project

---
+++ ACExam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ACExam
+//
+//  Created by martin on 2016/3/28.
+//  Copyright © 2016年 Frainbow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
